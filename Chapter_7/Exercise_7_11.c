e2bdb49b19506d9632b841555eaf2e272b8adeb7
lavr <lavr@localhost.localdomain>
2021-02-10 15:45:14 +0300

    release v0.0

---
+++ Chapter_7/Exercise_7_11.c
@@ -0,0 +1,21 @@
+// Глава 7 упражнение 11
+#include <stdio.h> // It was me, Dio!!!
+#define PRICE_ARTICHOKE 2.05
+#define PRICE_BEET 1.15
+#define PRICE_CARROT 1.09
+#define DISCOUNT 0.05
+#define PRICE_DELIVERY1 6.5
+#define PRICE_DELIVERY2 14.0
+#define PRICE_DELIVERY_INCREASE 0.5
+
+double Delivery();
+
+// Спршиваем вес, после q
+// Расчитываем стоимость + скидка
+// Расчитываем доставку
+// Выводим итог.
+int main()
+{
+    
+    return 0;
+}
