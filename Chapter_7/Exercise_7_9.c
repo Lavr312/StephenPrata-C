c3600c48eac04a2863ef5fd1d27a92d5acc0612f
Dmitrii <vodindim@yandex.ru>
2021-04-30 15:11:03 +0300

    Create Exercise_7_9.c

---
+++ Chapter_7/Exercise_7_9.c
@@ -0,0 +1,9 @@
+/*
+ *************************************************
+ *  Stephen Prata The C Programming Language    **
+ *      Lectures and Exercises 6th ed.          **
+ *                                              **
+ *  Programming Exercises 7.11                  **
+ * ***********************************************
+ */
+#include <stdio.h>
